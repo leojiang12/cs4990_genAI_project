24ea4576112459f048e3ec898bb7a4b9faa38909
Leo Jiang <leojiang@commander.unx.cpp.edu>
2025-04-25 11:41:29 -0700

    working train_ddp.py

--- src/models.py
+++ src/models.py
@@ -7,12 +7,12 @@ class UNetGenerator(nn.Module):
         super().__init__()
         # Encoder: downsampling
         self.down1 = nn.Sequential(nn.Conv2d(in_ch, features, 4, 2, 1), nn.LeakyReLU(0.2, inplace=False))
-        self.down2 = nn.Sequential(nn.Conv2d(features, features*2, 4, 2, 1), nn.BatchNorm2d(features*2), nn.LeakyReLU(0.2, inplace=False))
+        self.down2 = nn.Sequential(nn.Conv2d(features, features*2, 4, 2, 1), nn.BatchNorm2d(features*2, track_running_stats=False), nn.LeakyReLU(0.2, inplace=False))
         # … add more downs as desired …
         # Decoder: upsampling
-        self.up1   = nn.Sequential(nn.ConvTranspose2d(features*2, features, 4, 2, 1), nn.BatchNorm2d(features), nn.ReLU(inplace=False))
+        self.up1   = nn.Sequential(nn.ConvTranspose2d(features*2, features, 4, 2, 1), nn.BatchNorm2d(features, track_running_stats=False), nn.ReLU(inplace=False))
         # Final output
-        self.final = nn.Sequential(nn.ConvTranspose2d(features, out_ch, 4, 2, 1), nn.Tanh(inplace=False))
+        self.final = nn.Sequential(nn.ConvTranspose2d(features, out_ch, 4, 2, 1), nn.Tanh())
 
     def forward(self, x):
         d1 = self.down1(x)
@@ -26,7 +26,7 @@ class PatchDiscriminator(nn.Module):
         super().__init__()
         self.net = nn.Sequential(
             nn.Conv2d(in_ch, features, 4, 2, 1), nn.LeakyReLU(0.2, inplace=False),
-            nn.Conv2d(features, features*2, 4, 2, 1), nn.BatchNorm2d(features*2), nn.LeakyReLU(0.2, inplace=False),
+            nn.Conv2d(features, features*2, 4, 2, 1), nn.BatchNorm2d(features*2, track_running_stats=False), nn.LeakyReLU(0.2, inplace=False),
             nn.Conv2d(features*2, 1, 4, 1, 1)  # output 1-channel “real/fake” patch map
         )
     def forward(self, x, y):
