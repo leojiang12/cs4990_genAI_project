bcc8cfc883eb0d2dfe01bf758bd745b360afb2ed
Leo Jiang <leo99jiang@gmail.com>
2025-04-25 11:25:01 -0700

    inplace=False

--- src/models.py
+++ src/models.py
@@ -6,13 +6,13 @@ class UNetGenerator(nn.Module):
     def __init__(self, in_ch=3, out_ch=3, features=64):
         super().__init__()
         # Encoder: downsampling
-        self.down1 = nn.Sequential(nn.Conv2d(in_ch, features, 4, 2, 1), nn.LeakyReLU(0.2))
-        self.down2 = nn.Sequential(nn.Conv2d(features, features*2, 4, 2, 1), nn.BatchNorm2d(features*2), nn.LeakyReLU(0.2))
+        self.down1 = nn.Sequential(nn.Conv2d(in_ch, features, 4, 2, 1), nn.LeakyReLU(0.2, inplace=False))
+        self.down2 = nn.Sequential(nn.Conv2d(features, features*2, 4, 2, 1), nn.BatchNorm2d(features*2), nn.LeakyReLU(0.2, inplace=False))
         # … add more downs as desired …
         # Decoder: upsampling
-        self.up1   = nn.Sequential(nn.ConvTranspose2d(features*2, features, 4, 2, 1), nn.BatchNorm2d(features), nn.ReLU())
+        self.up1   = nn.Sequential(nn.ConvTranspose2d(features*2, features, 4, 2, 1), nn.BatchNorm2d(features), nn.ReLU(inplace=False))
         # Final output
-        self.final = nn.Sequential(nn.ConvTranspose2d(features, out_ch, 4, 2, 1), nn.Tanh())
+        self.final = nn.Sequential(nn.ConvTranspose2d(features, out_ch, 4, 2, 1), nn.Tanh(inplace=False))
 
     def forward(self, x):
         d1 = self.down1(x)
@@ -25,8 +25,8 @@ class PatchDiscriminator(nn.Module):
     def __init__(self, in_ch=6, features=64):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(in_ch, features, 4, 2, 1), nn.LeakyReLU(0.2),
-            nn.Conv2d(features, features*2, 4, 2, 1), nn.BatchNorm2d(features*2), nn.LeakyReLU(0.2),
+            nn.Conv2d(in_ch, features, 4, 2, 1), nn.LeakyReLU(0.2, inplace=False),
+            nn.Conv2d(features, features*2, 4, 2, 1), nn.BatchNorm2d(features*2), nn.LeakyReLU(0.2, inplace=False),
             nn.Conv2d(features*2, 1, 4, 1, 1)  # output 1-channel “real/fake” patch map
         )
     def forward(self, x, y):
