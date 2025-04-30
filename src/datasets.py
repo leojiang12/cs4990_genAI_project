a295d556f2bc5482cbb62d4b5820f33dcb4ec74c
Leo Jiang <leo99jiang@gmail.com>
2025-04-30 16:38:38 -0700

    modified train_controlnet_severity_ddp.py and datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -1,4 +1,4 @@
-# src/datasets/xbd_pair.py (or wherever your XBDPairDataset lives)
+# src/datasets/xbd_pair.py
 import os, glob, json
 from PIL import Image, ImageDraw
 from shapely import wkt
@@ -37,34 +37,31 @@ class XBDPairDataset(Dataset):
         return len(self.post_pngs)
 
     def __getitem__(self, idx):
-        # 2) get the full-res post‐disaster PNG
+        # 2) full-res post PNG
         post_png = self.post_pngs[idx]
-        # 3) derive the matching JSON (might not exist!)
-        rel_path = os.path.relpath(post_png, self.images_dir)
-        post_json = os.path.join(self.labels_dir,
-                                 rel_path.replace(".png", ".json"))
 
-        # try to load JSON; if missing, pretend “no features”
+        # 3) derive matching JSON (if missing → no damage)
+        rel      = os.path.relpath(post_png, self.images_dir)
+        post_json = os.path.join(self.labels_dir, rel.replace(".png", ".json"))
         if os.path.exists(post_json):
-            post_data = json.load(open(post_json))
-            features = post_data.get("features", {}).get("xy", [])
-            meta_post = post_data.get("metadata", {})
+            pd    = json.load(open(post_json))
+            feats = pd.get("features", {}).get("xy", [])
+            meta  = pd.get("metadata", {})
         else:
-            features = []
-            meta_post = {}
+            feats = []
+            meta  = {}
 
-        # 4) load pre/post images
+        # 4) load pre/post
         pre_png  = post_png.replace("_post_disaster.png", "_pre_disaster.png")
         post_img = Image.open(post_png).convert("RGB")
         pre_img  = Image.open(pre_png).convert("RGB")
 
-        # 5) rasterize polygons (if any) into a mask
+        # 5) rasterize polygons → mask
         mask = Image.new("L", post_img.size, 0)
         draw = ImageDraw.Draw(mask)
-        for feat in features:
-            poly   = wkt.loads(feat["wkt"])
-            coords = list(zip(poly.exterior.coords.xy[0],
-                              poly.exterior.coords.xy[1]))
+        for f in feats:
+            poly   = wkt.loads(f["wkt"])
+            coords = list(zip(poly.exterior.coords.xy[0], poly.exterior.coords.xy[1]))
             draw.polygon(coords, fill=1)
         del draw
 
@@ -72,27 +69,26 @@ class XBDPairDataset(Dataset):
         if self.annotate:
             ad = ImageDraw.Draw(pre_img,  "RGBA")
             bd = ImageDraw.Draw(post_img, "RGBA")
-            for feat in features:
-                sub   = feat["properties"].get("subtype", "no-damage")
-                color = damage_colors.get(sub, damage_colors["no-damage"])
-                poly   = wkt.loads(feat["wkt"])
-                coords = list(zip(poly.exterior.coords.xy[0],
-                                  poly.exterior.coords.xy[1]))
-                ad.polygon(coords, color)
-                bd.polygon(coords, color)
+            for f in feats:
+                sub   = f.get("properties",{}).get("subtype","no-damage")
+                col   = damage_colors.get(sub, damage_colors["no-damage"])
+                poly  = wkt.loads(f["wkt"])
+                coords= list(zip(poly.exterior.coords.xy[0], poly.exterior.coords.xy[1]))
+                ad.polygon(coords, col)
+                bd.polygon(coords, col)
             del ad, bd
 
         # 7) joint random crop
         i, j, h, w = T.RandomCrop.get_params(pre_img,
-                                             output_size=(self.crop_size, self.crop_size))
-        pre_crop  = F.crop(pre_img,  i, j, h, w)
-        post_crop = F.crop(post_img, i, j, h, w)
-        mask_crop = F.crop(mask,    i, j, h, w)
+                                             (self.crop_size, self.crop_size))
+        pre_c  = F.crop(pre_img,  i, j, h, w)
+        post_c = F.crop(post_img, i, j, h, w)
+        mask_c = F.crop(mask,    i, j, h, w)
 
-        # 8) to‐tensor & normalize
-        pre_t  = self.normalize(F.to_tensor(pre_crop))
-        post_t = self.normalize(F.to_tensor(post_crop))
-        mask_t = F.to_tensor(mask_crop)
+        # 8) to-tensor & normalize
+        pre_t  = self.normalize(F.to_tensor(pre_c))
+        post_t = self.normalize(F.to_tensor(post_c))
+        mask_t = F.to_tensor(mask_c)
 
         # 9) severity = fraction of damaged pixels
         severity = mask_t.mean()
@@ -102,5 +98,5 @@ class XBDPairDataset(Dataset):
             "post":     post_t,      # [3,H,W]
             "mask":     mask_t,      # [1,H,W]
             "severity": severity,    # scalar in [0,1]
-            "meta":     meta_post,   # metadata if available, else {}
+            "meta":     meta,        # metadata if any
         }
